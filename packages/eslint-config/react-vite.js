97f34a560029d297a97c1745cb4d32419061d0ba
user <user@userui-MacBookPro.local>
2026-01-31 18:06:02 +0900

    lint-config, tsconfig packages에서 관리하도록 세팅

---
+++ packages/eslint-config/react-vite.js
@@ -0,0 +1,20 @@
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import baseConfig from './base.js';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  ...baseConfig,
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+]);
